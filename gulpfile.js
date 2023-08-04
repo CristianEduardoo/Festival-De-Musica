3712efa810a5d92fb385f67bd00e8199c85e6344
CristianEduardoo <cj.94@hotmail.com>
2023-08-04 15:01:10 +0200

    retoques del header

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-const{src, dest, watch} = require("gulp");
+const{src, dest, watch} = require("gulp");//funcionalidades de la libreria de Gulp.
 const sass = require("gulp-sass")(require("sass"));//requerimos de nuestras dependencias instaladas.
 const plumber = require("gulp-plumber") //Imporartamos otra dependencia, para que no interrumpa nuestro workflow
 
+/*** COMPILAR EL ARCHIVO DE SASS CON 3 PASOS***/
 function css (done) {
-    src("src/scss/**/*.scss") //Identificamos el archivo sass, en este caso, todos
+    src("src/scss/**/*.scss") //1. Identificamos el archivo sass, en este caso todos
         .pipe(plumber())//--iria primero para que no interrumpa nuestro workflow
-        .pipe(sass()) // compilamos el archivo sass = const sass....
-        .pipe(dest("build/css")) //almacenamos en disco
+        .pipe(sass()) //2. compilamos el archivo sass = const sass....
+        .pipe(dest("build/css")) //3. almacenamos en disco
     
 
     done();//callback, que avisa a gulp que hemos terminado.
 }
 
-function dev (done){
+/*** Funcion para el watch ***/
+function dev (done){ 
     watch("src/scss/**/*.scss", css) //1º la ruta del archivi, 2ª la funcion a ejecutar
 
     done();
@@ -30,6 +32,6 @@ function tarea (done){
     done();// el callback, hacemos referencia para saber que termino de ejecutar nuestra funcion 
 }
 
-exports.tarea = tarea; //codigoNodeJS + nombre = función
-//asi es como mandamos a llamar por consola con el comando "npx gulp tarea" (nombre de la variable gulp = , parte izquierda)
+exports.tarea = tarea; //codigoNodeJS + <nombre> = función
+//asi es como mandamos a llamar por consola con el comando "npx gulp tarea" (tarea = exports.tarea)
 */
